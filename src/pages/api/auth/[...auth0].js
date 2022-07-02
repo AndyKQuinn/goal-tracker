0ef9c57f968fc8869b09b5e4583e9fed17959015
Andy Quinn <andy_guice@hotmail.com>
2022-07-02 10:35:38 -0500

    fix: clean up auth redirect, remove unused code and config

--- src/pages/api/auth/[...auth0].js
+++ src/pages/api/auth/[...auth0].js
@@ -1,16 +1,3 @@
-import { handleAuth, handleCallback } from '@auth0/nextjs-auth0';
+import { handleAuth } from '@auth0/nextjs-auth0';
 
-const afterCallback = (session) => {
-  // console.log("Session: ", session)
-  return session
-}
-
-export default handleAuth({
-  async callback(req, res) {
-    try {
-      await handleCallback(req, res, { afterCallback });
-    } catch (error) {
-      res.status(error.status || 500).end(error.message)
-    }
-  }
-});
+export default handleAuth();
